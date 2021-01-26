963e3b0f0fae8c0965003f426cc4dddc555b452a
NickSpangler <Spangleyman@mac.com>
2021-01-26 11:20:06 -0500

    Done.

--- index.js
+++ index.js
@@ -62,3 +62,52 @@ function merge(left, right) {
     }
     return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex))
 }
+
+function binaryMatch(array, target) {
+    let start = 0, end = array.length - 1
+    while (start <= end) {
+        let middle = Math.floor((start + end)/2)
+        if (target === array[middle]) return true;
+
+        if (target > array[middle]) return binarySearch(array.slice(middle + 1), target)
+
+        if (target < array[middle]) return binarySearch(array.slice(0, middle - 1), target)
+    }
+    return false;
+}
+
+// function hashTwoSum(array, n) {
+//     let hash = createHash(array)
+//     console.log(Object.keys(hash))
+//     console.log(Object.values(hash))
+// }
+
+// function createHash(array) {
+//     let hash = {}
+//     array.forEach(num => hash[`${num}`] = num)
+//     return hash;
+// }
+
+const hashTwoSum = (arr, target) => {
+    let numObject = {};
+    let results = [];
+	for (let i = 0; i < arr.length; i++) {
+        let thisNum = arr[i];
+        if (!numObject.hasOwnProperty(thisNum)) {
+		    numObject[thisNum] = i;
+        } else {
+            numObject[thisNum] = [numObject[thisNum], i]
+        }
+    }
+    console.log(numObject)
+	for (var i = 0; i < arr.length; i++) {
+        let diff = target - arr[i];
+        // if (diff === arr[i] && numObject[arr[i]].length > 1) {
+        //     results.push([arr[i], arr[i]])
+        // }
+		if (numObject.hasOwnProperty(diff) && numObject[diff] !== i) {
+			results.push([arr[i], arr[numObject[diff]]]);
+		}
+    }
+    return results;
+}
