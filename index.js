0ea161ff32756c0ece01d59a5d4501c3a1ea0423
NickSpangler <Spangleyman@mac.com>
2021-01-25 19:22:52 -0500

    solve binarySearchTwoSum()

--- index.js
+++ index.js
@@ -13,8 +13,29 @@ function bruteForceTwoSum(array, n) {
 }
 
 function binarySearchTwoSum(array, n) {
+    let pairs = []
     let sortedArray = mergeSort(array);
-    
+    while (sortedArray.length > 1) {
+        let current = sortedArray.shift();
+        let target = n - current;
+        if (binarySearch(sortedArray, target)) {
+            pairs.push([current, target])
+        }
+    }
+    return pairs;
+}
+
+function binarySearch(array, target) {
+    let start = 0, end = array.length
+    while (start < end) {
+        let middle = Math.floor((start + end)/2)
+        if (target === array[middle]) return true;
+
+        if (target > array[middle]) return binarySearch(array.slice(middle + 1), target)
+
+        if (target < array[middle]) return binarySearch(array.slice(0, middle - 1), target)
+    }
+    return false;
 }
 
 function mergeSort(array) {
@@ -22,7 +43,7 @@ function mergeSort(array) {
         return array;
     }
     const middle = Math.floor(array.length/2)
-    const left = array.slice(0, midddle);
+    const left = array.slice(0, middle);
     const right = array.slice(middle)
 
     return merge(mergeSort(left), mergeSort(right))
